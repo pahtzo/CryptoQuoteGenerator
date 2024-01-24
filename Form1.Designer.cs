b42fa6fe0098e40c601f9d1fd478b090c18f2f26
pahtzo <pahtzo@users.noreply.github.com>
2024-01-23 20:17:30 -0500

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Alphabet = new System.Windows.Forms.TextBox();
+            this.btn_scramble = new System.Windows.Forms.Button();
+            this.textBox_Key = new System.Windows.Forms.TextBox();
+            this.textBox_PlainText = new System.Windows.Forms.TextBox();
+            this.textBox_CryptedText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_savesolution = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_Alphabet
+            // 
+            this.textBox_Alphabet.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Alphabet.Location = new System.Drawing.Point(12, 15);
+            this.textBox_Alphabet.Name = "textBox_Alphabet";
+            this.textBox_Alphabet.ReadOnly = true;
+            this.textBox_Alphabet.Size = new System.Drawing.Size(508, 23);
+            this.textBox_Alphabet.TabIndex = 3;
+            this.textBox_Alphabet.TabStop = false;
+            // 
+            // btn_scramble
+            // 
+            this.btn_scramble.Location = new System.Drawing.Point(639, 12);
+            this.btn_scramble.Name = "btn_scramble";
+            this.btn_scramble.Size = new System.Drawing.Size(92, 23);
+            this.btn_scramble.TabIndex = 1;
+            this.btn_scramble.Text = "Scramble";
+            this.btn_scramble.UseVisualStyleBackColor = true;
+            this.btn_scramble.Click += new System.EventHandler(this.btn_scramble_Click);
+            // 
+            // textBox_Key
+            // 
+            this.textBox_Key.AcceptsReturn = true;
+            this.textBox_Key.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Key.Location = new System.Drawing.Point(13, 47);
+            this.textBox_Key.Name = "textBox_Key";
+            this.textBox_Key.ReadOnly = true;
+            this.textBox_Key.Size = new System.Drawing.Size(507, 23);
+            this.textBox_Key.TabIndex = 4;
+            this.textBox_Key.TabStop = false;
+            // 
+            // textBox_PlainText
+            // 
+            this.textBox_PlainText.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_PlainText.Location = new System.Drawing.Point(16, 100);
+            this.textBox_PlainText.Multiline = true;
+            this.textBox_PlainText.Name = "textBox_PlainText";
+            this.textBox_PlainText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_PlainText.Size = new System.Drawing.Size(715, 230);
+            this.textBox_PlainText.TabIndex = 0;
+            // 
+            // textBox_CryptedText
+            // 
+            this.textBox_CryptedText.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_CryptedText.Location = new System.Drawing.Point(16, 354);
+            this.textBox_CryptedText.Multiline = true;
+            this.textBox_CryptedText.Name = "textBox_CryptedText";
+            this.textBox_CryptedText.ReadOnly = true;
+            this.textBox_CryptedText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_CryptedText.Size = new System.Drawing.Size(715, 230);
+            this.textBox_CryptedText.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(530, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Alphabet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(530, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Substitution Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Plaintext";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ciphertext";
+            // 
+            // btn_savesolution
+            // 
+            this.btn_savesolution.Location = new System.Drawing.Point(639, 44);
+            this.btn_savesolution.Name = "btn_savesolution";
+            this.btn_savesolution.Size = new System.Drawing.Size(92, 23);
+            this.btn_savesolution.TabIndex = 2;
+            this.btn_savesolution.Text = "Save Solution";
+            this.btn_savesolution.UseVisualStyleBackColor = true;
+            this.btn_savesolution.Click += new System.EventHandler(this.btn_savesolution_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 600);
+            this.Controls.Add(this.btn_savesolution);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_CryptedText);
+            this.Controls.Add(this.textBox_PlainText);
+            this.Controls.Add(this.textBox_Key);
+            this.Controls.Add(this.btn_scramble);
+            this.Controls.Add(this.textBox_Alphabet);
+            this.Name = "Form1";
+            this.Text = "CryptoQuote Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Alphabet;
+        private System.Windows.Forms.Button btn_scramble;
+        private System.Windows.Forms.TextBox textBox_Key;
+        private System.Windows.Forms.TextBox textBox_PlainText;
+        private System.Windows.Forms.TextBox textBox_CryptedText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_savesolution;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿/*
+ * CryptoQuote Generator for creating cryptoquotes like those found in newspapers and word puzzle books.
+ * Creates two HTML files, one with just the puzzle and reference alphabet to solve
+ * and one with the puzzle, solution, and substitution alphabet.
+ * 
+ * Nick DeBaggis - 2014, 2024
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        static string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string plain = "";
+        string plain2 = "";
+        string crypted2 = "";
+        string shuffled = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_Alphabet.Text = alphabet;
+            textBox_Key.Text = alphabet;
+            btn_savesolution.Enabled = false;
+        }
+
+        /*
+         * Shuffle the alphabet key into random order.
+         */
+        private static string Shuffle(string list)
+        {
+            StringBuilder sb = new StringBuilder();
+            Random randomnumber = new Random();
+            List<char> chars = new List<char>(list);
+            while (chars.Count > 0)
+            {
+                int index = randomnumber.Next(chars.Count);
+                sb.Append(chars[index]);
+                chars.RemoveAt(index);
+            }
+            return sb.ToString();
+        }
+
+        /*
+         * Check the shuffled alphabet "key" against the ordered alphabet.
+         * If the key and alphabet have any matching letters then return false
+         * to force another alphabet key shuffle.
+         */
+        static private bool checkkey(string key)
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                if (alphabet[i] == key[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void btn_scramble_Click(object sender, EventArgs e)
+        {
+            shuffled = Shuffle(key);
+
+            // Loop through to ensure the randomized alphabet key has no matching letters in the ordered alphabet.
+            // i.e. A != A, B != B, ...
+            // This is a bit slow but since we aren't processing batches of cryptoquotes it shouldn't be noticable.
+            // Based on code profiling, on average it takes just over 3 shuffles per scramble click
+            // to obtain a key where each letter substitution is unique.
+
+            while (checkkey(shuffled) == false)
+            {
+                shuffled = Shuffle(key);
+            }
+
+            textBox_Key.Text = shuffled;
+            textBox_PlainText.Text = textBox_PlainText.Text.ToUpper();
+            plain = textBox_PlainText.Text;
+
+            char[] crypted = new char[plain.Length];
+            StringBuilder sb = new StringBuilder();
+
+            // Perform the cryptoquote substitutions.
+            for (int i = 0; i < plain.Length; i++)
+            {
+                if (plain[i] >= 'A' && plain[i] <= 'Z')
+                {
+                    crypted[i] = shuffled[plain[i] - 'A'];
+                }
+                else
+                    crypted[i] = plain[i];
+
+            }
+            for (int i = 0; i < crypted.Length; i++)
+            {
+                sb.Append(crypted[i]);
+            }
+            textBox_CryptedText.Text = sb.ToString();
+
+            crypted2 = textBox_CryptedText.Text.Replace("\n", "<br>");
+            plain2 = textBox_PlainText.Text.Replace("\n", "<br>");
+            btn_savesolution.Enabled = true;
+        }
+
+        private void btn_savesolution_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            if (fb.ShowDialog() != DialogResult.OK) return;
+
+            string path = fb.SelectedPath;
+            string solutionfilename;
+            string puzzlefilename;
+            DateTime dt = DateTime.Now;
+
+            solutionfilename = path + "\\" + dt.ToString("yyyyMMdd_HHmmss") + "-solution.html";
+            puzzlefilename = path + "\\" + dt.ToString("yyyyMMdd_HHmmss") + "-puzzle.html";
+
+            savesolution(solutionfilename);
+            savepuzzle(puzzlefilename);
+            MessageBox.Show("Saved cryptoquote puzzle and solution to:\n\n" + puzzlefilename + "\n" + solutionfilename);
+        }
+
+        private void savesolution(String filename)
+        {
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(filename);
+
+            sw.Write(
+            "<!DOCTYPE html>" +
+            "<html>" +
+            "<head>" +
+            "<style>" +
+            "div.crypt {" +
+            "letter-spacing: 8px;" +
+            "font-family:courier, \"courier new\", monospace;" +
+            "font-size:large;" +
+            "line-height: 350%;" +
+            "}" +
+            "</style>" +
+            "</head>" +
+            "<title>CryptoQuote Solution</title>" +
+            "<body>" +
+            "<div class=\"crypt\">"
+            );
+
+            sw.WriteLine("<br>" + alphabet + "<br>");
+            sw.WriteLine(shuffled + "<br><br>");
+            sw.WriteLine("</div>");
+            sw.WriteLine("<div class=\"crypt\">");
+            sw.WriteLine("<hr>");
+            sw.WriteLine(plain2 + "<br>");
+            sw.WriteLine("<hr>");
+            sw.Write(crypted2 + "<br>");
+
+            sw.Write(
+            "</div>" +
+            "</body>" +
+            "</html>"
+            );
+
+            sw.Close();
+        }
+
+        private void savepuzzle(String filename)
+        {
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(filename);
+
+            sw.Write(
+            "<!DOCTYPE html>" +
+            "<html>" +
+            "<head>" +
+            "<style>" +
+            "div.crypt {" +
+            "letter-spacing: 8px;" +
+            "font-family:courier, \"courier new\", monospace;" +
+            "font-size:large;" +
+            "line-height: 350%;" +
+            "}" +
+            "</style>" +
+            "</head>" +
+            "<title>CryptoQuote Puzzle</title>" +
+            "<body>" +
+            "<div class=\"crypt\">"
+            );
+
+            sw.WriteLine("<br>" + alphabet + "<br><br>");
+            sw.Write(crypted2 + "<br>");
+
+            sw.Write(
+            "</div>" +
+            "</body>" +
+            "</html>"
+            );
+
+            sw.Close();
+        }
+    }
+}
